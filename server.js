04a302280ce1b9e4917891e0f3e5ff7f31d957b7
mikedichello <michael.dichello@gmail.com>
2019-12-19 22:24:05 -0600

    initial mvc build

---
+++ server.js
@@ -0,0 +1,29 @@
+//DEPENDENCIES
+const express = require('express');
+const mongoose = require('./db/connection.js');
+const app = express();
+const db = mongoose.connection;
+
+//ENVIRONMENT VARIABLES
+const PORT = process.env.PORT || 3000;
+
+//ERROR/DISCONNECTION
+db.on('error', err => console.log(err.message + 'is Mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'))
+
+//ROUTES
+const plotsController = require('./controllers/plots.js');
+app.use('/plots', plotsController);
+
+app.get('*', (req, res) => {
+    res.status(404).json('Sorry, page not found')
+})
+
+app.listen(PORT, () => {
+    console.log('Let\'s get things done on port', PORT)
+})
