0409be554aaa832db24c2f35f3a487978b1aee76
Leonardo Santana <hi@leonsantana.com>
2019-12-31 17:14:24 -0500

    plant chart and plot component from David

---
+++ frontEndSrc/js/components/PlantChart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PlantChart extends Component {
+	render() {
+		return (
+			<div>
+				<p>This is Plant Chart</p>
+			</div>
+		);
+	}
+}
