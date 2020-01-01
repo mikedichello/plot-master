a0bc84e4b33aad6b9e1c4a0c2ec2af4c704887d1
mikedichello <michael.dichello@gmail.com>
2020-01-01 15:02:25 -0600

    added state and list of crops with state change

--- frontEndSrc/js/components/ViewAllPlants.js
+++ frontEndSrc/js/components/ViewAllPlants.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 export default class ViewAllPlants extends Component {
+	state = {
+		allCrops: Crops,
+		currentCrop: {}
+	}
+
+
 	render() {
 		return (
 			<div>
 				<h1>View All Plants</h1>
+				<ul>
+					{this.state.allCrops.map((Crops, index) => {
+						return(
+							<li  onClick={() => this.setState({currentCrop: Crops})}>{Crops.name}</li>
+						)
+					})}
+				</ul>
 			</div>
 		);
 	}
