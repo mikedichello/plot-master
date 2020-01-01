daf25a1a838730d1ca5fcfd2c99c88a8c2058516
Leonardo Santana <hi@leonsantana.com>
2019-12-31 20:14:54 -0500

    first try at getting the api call fix, getting a 404 error currently

--- backEndSrc/controllers/plots.js
+++ backEndSrc/controllers/plots.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Plots = require('../models/Plot');
+const Plots = require('../models/Plots');
 
 //READ
 router.get('/', (req, res) => {
