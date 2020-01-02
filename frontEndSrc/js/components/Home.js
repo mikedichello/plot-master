6a2bc846c4a1ff2892b658b88e1ced9dc32ccaf1
Jane Nath <53629440+janenath@users.noreply.github.com>
2020-01-02 09:09:42 -0800

    Adding content to Home.js

--- frontEndSrc/js/components/Home.js
+++ frontEndSrc/js/components/Home.js
@@ -4,7 +4,9 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div>
-				<h1>This is Home</h1>
+				<h1>Welcome to Plot Master</h1>
+				<img src='../../public/img/sunset_beds.jpg' width='500px' alt='garden beds'/>
+				<p>Plot Master is the ultimate garden design tool for any planting space. Create your custom-sized plot, choose your plants, and view a chart of suggested planting and harvesting times based on your climate. Whether you are an amateur home gardener or a seasoned professional, Plot Master will keep your plot organized so you can get growing with confidence!</p>
 			</div>
 		);
 	}
