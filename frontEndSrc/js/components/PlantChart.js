12561cd2b384097cac23c5331a0e3743ed95421f
David Hall <davidhall@Davids-MacBook-Pro.local>
2020-01-02 23:14:03 -0500

    planting chart up and running

--- frontEndSrc/js/components/PlantChart.js
+++ frontEndSrc/js/components/PlantChart.js
@@ -1,53 +1,66 @@
 import React, { Component } from 'react';
 import Crops from '../../../public/js/Crops.js'
+const months = ['January','February','March','April','May',
+			'June','July','August','September','October',
+			'November','December']
 
 export default class PlantingChart extends Component{
 		state = {
-			allPlants: Crops,
+			crops: Crops,
+			currentDetails:null,
+			germinate:2,
+			daysToMaturity:0,
+			harvest:0
+		}
+		plantDetails = (index) => {
+			event.preventDefault()
+			let crop = Crops[index]
+			let daysToMaturity = this.state.germinate+Math.round(crop.daysToMaturity/30)
+			let harvest = this.state.germinate+Math.round(crop.daysToMaturity/30)
+			this.setState({
+				currentDetails:crop,
+				daysToMaturity: daysToMaturity,
+				harvest:harvest
+			})
+
 		}
 
 	render() { 
 			return (
-				<div className = 'chartContainer'>
-					<h2>My Plant Chart</h2>
-					<table>
-						<tr>
-							<th>Plant Name</th>
-							<th>January</th>
-							<th>February</th>
-							<th>March</th>
-							<th>April</th>
-							<th>May</th>
-							<th>June</th>
-							<th>July</th>
-							<th>August</th>
-							<th>September</th>
-							<th>October</th>
-							<th>November</th>
-							<th>December</th>
-						</tr>
-					{this.state.allPlants.map((plant, i) => {
-						return(
-							<tr>
-								<td onClick={this.viewPlantDetails}>{plant.name}</td>
-								<td className={plant.name + " January"} ></td>
-								<td className={plant.name + " February"} ></td>
-								<td className={plant.name + " March"} ></td>
-								<td className={plant.name + " April"} ></td>
-								<td className={plant.name + " May"} ></td>
-								<td className={plant.name + " June"} ></td>
-								<td className={plant.name + " July"} ></td>
-								<td className={plant.name + " August"} ></td>
-								<td className={plant.name + " September"} ></td>
-								<td className={plant.name + " October"} ></td>
-								<td className={plant.name + " November"} ></td>
-								<td className={plant.name + " December"} ></td>
-							</tr>
-						)
-					}
-					)}
-					</table>
+				<div>
+					<div>Click plant name for details</div>
+				
+
+				{this.state.crops.map((crop,index) => {
+					return <div onClick={()=> this.plantDetails(index)}>{crop.name}</div>
+				})}
+				{this.state.currentDetails!==null ?
+				<div style={{width:"960px"}}>
+					<div style={{width:"960px",display:"flex"}} className="months">
+					{months.map((month,index) => {
+						return <div style={{width:"80px"}}className={month}>{month}</div>
+					})}
+					</div>
+					<div style={{width:"960px",display:"flex"}}>
+					{months.map((month,index) => {
+						return <div style={{width:"80px",height:"20px"}}className={(this.state.germinate===index ? "germinate" : '' )}></div>
+					})}
+					</div>
+					<div style={{width:"960px",display:"flex"}}>
+					{months.map((month,index) => {
+						return <div style={{width:"80px",height:"20px"}}className={(((this.state.germinate+1<=index) && (this.state.daysToMaturity>=index)) ? "growth" : '' )}></div>
+					})}
+					</div>
+					<div style={{width:"960px",display:"flex"}}>
+					{months.map((month,index) => {
+						return <div style={{width:"80px",height:"20px"}}className={(((this.state.daysToMaturity+1<=index) && (index<=8)) ? "harvest" : '' )}></div>
+					})}
+					</div>
+				</div> : <div></div>}
+					
 				</div>
+				
 			)
 	}
 }
+	
